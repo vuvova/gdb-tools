1983c2af6fa00251ec8f02949e42d43360c58dd5
Sergei Golubchik <vuvova@gmail.com>
2017-05-09 01:00:59 +0200

    document pretty_printer.py

--- pretty_printer.py
+++ pretty_printer.py
@@ -1,3 +1,5 @@
+"""a helper for easy creation of gdb pretty-printers"""
+
 import gdb.printing
 
 # in python2 gdb.Value can only be converted to long(), python3 only has int()
@@ -5,7 +7,29 @@ try: a=long(1)
 except: long=int
 
 def PrettyPrinter(arg):
+    """@PrettyPrinter decorator.
 
+    With a @PrettyPrinter decorator one only needs to write a function
+    that takes gdb.Value as an argument and returns a string to be
+    shown by gdb.
+
+    Typical usage:
+
+        @PrettyPrinter
+        def some_typename(val):
+            <convert val to a string and return it>
+
+    This creates all necessary classes and register a pretty-printer
+    for the type "some_typename", be it a typedef'ed type name or
+    the real underlying type with all typedef's resolved. It also
+    creates a pretty-printer for a pointer to some_typename.
+
+    When a type name is not a valid Python identifier, one can use
+
+        @PrettyPrinter("real complex type name")
+        def does_not_matter(val):
+            <convert val to a string and return it>
+    """
     name = getattr(arg, '__name__', arg)
 
     def PrettyPrinterWrapperWrapperWrapper(func):
