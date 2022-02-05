3e01404505d1f2b8627d1f2fdeeaf3dab833a7c7
Sergei Golubchik <vuvova@gmail.com>
2022-02-05 23:23:44 +0100

    redo the packaging, use build
    `python setup.py sdist upload` no longer works

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from subprocess import Popen, PIPE
+from os import execlp
+from os.path import dirname
+import re
+
+stem=__file__.rstrip('py')
+with open(stem+'gdb', 'r') as fg, open(stem+'in', 'w') as fi, open(stem+'out', 'w') as fo:
+    for l in fg:
+        if l.startswith('(gdb) '):
+            fi.write(l[6:])
+        else:
+            fo.write(l)
+p=Popen(['gdb', '-batch', '-n', '-x', 'test.in'], cwd=dirname(stem), stdout=PIPE, stderr=PIPE, universal_newlines=True)
+(o,e)=p.communicate()
+if e: raise Exception(e)
+o = re.sub(r'(=.*) 0x[0-9a-f]+', r'\1 0xXXXXX', o)
+o = re.sub(r'Temporary breakpoint 1 at .*\n', '', o)
+with open(stem+'reject', 'w') as f: f.write(o)
+execlp('diff', 'diff', '-us', stem+'out', stem+'reject')
